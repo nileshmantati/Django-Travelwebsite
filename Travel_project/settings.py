a21fc82836a03527804245904fd31585fbac7c25
Sanjay Mantati <sanjayms9510@gmail.com>
2026-02-02 22:07:38 +0530

    comment

--- Travel_project/settings.py
+++ Travel_project/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+# import cloudinary
+# import cloudinary.uploader
+# import cloudinary.api
 from pathlib import Path
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cp5m_d5m-hrtdtyi40q428hl5g&o0$63k5yd)v&z_jp@o603=j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
